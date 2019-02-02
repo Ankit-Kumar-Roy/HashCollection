31aaeb23e0c907407ae03c38a5949bb1de0ba20e
Ankit-Kumar-Roy <ankitroy55@outlook.com>
2019-02-02 17:11:57 +0530

    Add project files.

---
+++ HashCollection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ht = new Hashtable();
+            ht.Add("EId", 1010);
+            ht.Add("Ename", "Scott");
+            ht.Add("Job", "Manager");
+            ht.Add("Salary", 25000.00);
+            ht.Add("MngId", 1002);
+            ht.Add("Phone", "6203725170");
+            ht.Add("Email", "royankit2011@gmail.com");
+            ht.Add("Dname", "Sales");
+            ht.Add("Location", "Mumbai");
+            ht.Add("Did", 30);
+
+            foreach (var key in ht.Keys)
+            {                
+                Console.WriteLine("{0}:  {1}", key, ht[key]);
+            }
+        }
+    }
+}
